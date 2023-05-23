bb37eb31c84963abfe0d6a5df697434422136cec
Peter Parker <105187918+Adithyan06@users.noreply.github.com>
2023-05-23 20:47:33 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, jsonify, render_template
+
+import sqlite3
+
+app = Flask(name)
+
+@app.route('/')
+
+def index():
+
+    return render_template('index.html')
+
+@app.route('/movies')
+
+def get_movies():
+
+    conn = sqlite3.connect('movies.db')
+
+    cursor = conn.cursor()
+
+    cursor.execute('SELECT * FROM movies')
+
+    movies = cursor.fetchall()
+
+    conn.close()
+
+    return jsonify(movies)
+
+if name == 'main':
+
+    app.run()
