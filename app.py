da57bf477a308ba0e276b5a3948bd80771c67080
Peter Parker <105187918+Adithyan06@users.noreply.github.com>
2023-05-24 10:42:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, render_template
 
 import sqlite3
 
-app = Flask(name)
+app = Flask(__name__)
 
 @app.route('/')
 
@@ -26,6 +26,6 @@ def get_movies():
 
     return jsonify(movies)
 
-if name == 'main':
+if __name__ == '__main__':
 
     app.run()
