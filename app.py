a4a236cefc9b07ef4121cb61fa1b3ebf354a09df
Adithyan <105187918+Adithyan06@users.noreply.github.com>
2024-12-07 22:32:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,16 @@
 from flask import Flask, jsonify, render_template
-
 import sqlite3
-
 app = Flask(__name__)
-
 @app.route('/')
-
 def index():
-
     return render_template('index.html')
-
 @app.route('/movies')
-
 def get_movies():
-
     conn = sqlite3.connect('movies.db')
-
     cursor = conn.cursor()
-
     cursor.execute('SELECT * FROM movies')
-
     movies = cursor.fetchall()
-
     conn.close()
-
     return jsonify(movies)
-
 if __name__ == '__main__':
-
     app.run()
