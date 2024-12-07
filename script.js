7cdaa1e0d64577e7965403a4cd5fa7873c7a02a8
Adithyan <105187918+Adithyan06@users.noreply.github.com>
2024-12-07 22:33:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,14 @@
 window.addEventListener('load', function() {
-
     fetch('/movies')
-
         .then(response => response.json())
-
         .then(data => {
-
             const moviesDiv = document.getElementById('movies');
-
             data.forEach(movie => {
-
                 const movieElement = document.createElement('div');
-
                 movieElement.innerHTML = <h2>${movie.title}</h2>
-
                                           <p>Genre: ${movie.genre}</p>
-
                                           <p>Release Year: ${movie.release_year}</p>;
-
                 moviesDiv.appendChild(movieElement);
-
             });
-
-        });
-
+      });
 });
